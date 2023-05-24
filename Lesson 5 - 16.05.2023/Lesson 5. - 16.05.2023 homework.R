425f150d0ca3739b8e4a4cade124a18cc288633c
Alessandro1984 <bramucci.alessandro@gmail.com>
2023-05-24 18:13:16 +0200

    Corrections

--- Lesson 5 - 16.05.2023/Lesson 5. - 16.05.2023 homework.R
+++ Lesson 5 - 16.05.2023/Lesson 5. - 16.05.2023 homework.R
@@ -1,6 +1,8 @@
+rm(list = ls())
+
 library(readxl)
 
-data_real_gdp <- read_excel("data_real_gdp.xlsx", 
+data_real_gdp <- read_excel("Lesson 5 - 16.05.2023/data_real_gdp.xlsx", 
                             sheet = "data", 
                             skip = 3)
 
@@ -26,27 +28,19 @@ for (i in 1:length(countries)) {
        ylab = "ln GDP")
   
   y <- data_real_gdp$value[data_real_gdp$country == countries[i]]
-    
+
   x <- data_real_gdp$time_trend[data_real_gdp$country == countries[i]]
-  
+
   reg <- lm(log(y) ~ x + I(x^2))
-  
+   
   lines(data_real_gdp$year[data_real_gdp$country == countries[i]],
         reg$fitted.values,
         col = cols[i],
         lty = 1)
   
-  # legend("topleft",
-  #        legend = paste0("MPC:",round(reg$coefficients[2], digits = 2)),
-  #        col = cols[i], 
-  #        lty = 1, 
-  #        lwd = 1, 
-  #        bty = "n", 
-  #        cex = 0.8)
-  
-  legend("bottomright",                                       
-         expression(hat(lnGDP) == hat(beta)["0"] + hat(beta)["1"]%.%t + hat(beta)["2"]%.%t^2),         
-         col = cols[i],           
+  legend("bottomright",
+         expression(hat(lnGDP) == hat(beta)["0"] + hat(beta)["1"]%.%t + hat(beta)["2"]%.%t^2),
+         col = cols[i],
          lty = 1,
          cex = 1,
          bty = "n")
