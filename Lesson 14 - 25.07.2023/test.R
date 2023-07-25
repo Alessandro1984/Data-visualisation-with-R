918e64d2fe1b0a38b462ce0631bc2f1f2d774523
Alessandro1984 <bramucci.alessandro@gmail.com>
2023-07-25 12:29:18 +0200

    Lesson 14

---
+++ Lesson 14 - 25.07.2023/test.R
@@ -0,0 +1,32 @@
+library(gapminder)
+library(dplyr)
+
+data_2007 <- gapminder %>% 
+  filter(year == 2007)
+
+robust_scalar <- function(x) {
+  (x - median(x)) / IQR(x)
+}
+
+minmax <- function(x) {
+  (x - min(x)) / (max(x) - min(x))
+}
+
+gr_rate <- function(x) {
+  c(NA, diff(x)) / lag(x) * 100
+}
+
+data_2007_scaled <- data_2007 %>% 
+  mutate(across(where(is.numeric), minmax))
+
+plot(as.data.frame(data_2007_scaled[, -c(1,2,3)]))
+
+fitK <- kmeans(data_2007_scaled[, -c(1,2,3)], 4)
+
+fitK
+
+str(fitK)
+
+fitK$cluster
+
+plot(data_2007_scaled[, -c(1,2,3)], col = fitK$cluster)
